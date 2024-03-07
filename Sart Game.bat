27eb31cc5b280a341120e32c972e64a076502177
leJacob_ <119848839+lejacobdev@users.noreply.github.com>
2024-03-07 13:20:26 +0100

    Add files via upload

---
+++ Sart Game.bat
@@ -0,0 +1,31 @@
+@echo off
+Title Starter
+set /p Farbe= <color.txt
+color %Farbe%
+echo Game is starting in a few seconds
+cls
+echo Loading
+timeout 1 > nul
+cls
+echo Loading.
+timeout 1 > nul
+cls
+echo Loading..
+timeout 1 > nul
+cls
+echo Loading...
+timeout 1 > nul
+cls
+echo Loading
+timeout 1 > nul
+cls
+echo Loading.
+timeout 1 > nul
+cls
+echo Loading..
+timeout 1 > nul
+cls
+echo Loading...
+timeout 1 > nul
+cls
+call Menu.bat
