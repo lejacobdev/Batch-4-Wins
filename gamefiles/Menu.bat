ce87c1bb97b6b26f7f7114adb35ce64f671cbae8
leJacob_ <119848839+lejacobdev@users.noreply.github.com>
2024-03-07 13:09:25 +0100

    Update Menu.bat

--- gamefiles/Menu.bat
+++ gamefiles/Menu.bat
@@ -30,7 +30,7 @@ if errorlevel 0 goto GUIMenu
 if "%men1%"=="(" goto Start
 if "%men3%"=="(" goto Einstellungen
 if "%men5%"=="(" goto Credits
-if "%men7%"=="(" exit
+if "%men7%"=="(" goto eof
 goto GUIMenu
 
 :GUIMenurunter
